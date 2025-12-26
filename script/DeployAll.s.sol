4568be6adf2e6a06490ea137c1afa6fdd949ff64
mfw78 <mfw78@nxm.rs>
2025-12-26 12:57:17 +0000

    feat(deploy): add SwapPriceOracle deployment for SWAP protocol
    Deploy both StoragePriceOracle and SwapPriceOracle:
    - StoragePriceOracle: for postage stamp pricing (storage incentives)
    - SwapPriceOracle: for SWAP protocol cheque payments
    
    Bee nodes require SwapPriceOracle at BEE_PRICE_ORACLE_ADDRESS for the
    getPrice() function used in bandwidth payment negotiations.
    
    Also makes NETWORK_ID configurable via environment variable.

--- script/DeployAll.s.sol
+++ script/DeployAll.s.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.30;
 import { Script, console } from "forge-std/Script.sol";
 import { TestToken } from "../src/common/TestToken.sol";
 import { PostageStamp } from "../src/incentives/PostageStamp.sol";
-import { PriceOracle } from "../src/incentives/StoragePriceOracle.sol";
+import { PriceOracle as StoragePriceOracle } from "../src/incentives/StoragePriceOracle.sol";
+import { PriceOracle as SwapPriceOracle } from "../src/swap/SwapPriceOracle.sol";
 import { StakeRegistry } from "../src/incentives/Staking.sol";
 import { Redistribution } from "../src/incentives/Redistribution.sol";
 import { SimpleSwapFactory } from "../src/swap/SimpleSwapFactory.sol";
@@ -13,26 +14,37 @@ import { SimpleSwapFactory } from "../src/swap/SimpleSwapFactory.sol";
  * @title DeployAll
  * @notice Deploys all Swarm contracts in the correct order
  * @dev Run with: forge script script/DeployAll.s.sol:DeployAll --rpc-url <rpc_url> --broadcast
+ *
+ * Note: Two price oracles are deployed:
+ * - StoragePriceOracle: For postage stamp pricing (storage incentives)
+ * - SwapPriceOracle: For SWAP protocol cheque payments (BEE_PRICE_ORACLE_ADDRESS)
  */
 contract DeployAll is Script {
     // Deployment configuration
     uint8 public constant MIN_BUCKET_DEPTH = 16;
-    uint64 public constant NETWORK_ID = 1; // Mainnet
+
+    // SwapPriceOracle configuration
+    // Initial price in PLUR per accounting unit (default from beekeeper)
+    uint256 public constant SWAP_INITIAL_PRICE = 10000;
+    // Initial cheque value deduction in PLUR (default from beekeeper)
+    uint256 public constant SWAP_CHEQUE_VALUE_DEDUCTION = 1;
 
     // Deployed contract addresses
     address public token;
     address public postageStamp;
-    address public priceOracle;
+    address public storagePriceOracle;
+    address public swapPriceOracle;
     address public stakeRegistry;
     address public redistribution;
     address public swapFactory;
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        uint64 networkId = uint64(vm.envOr("NETWORK_ID", uint256(1)));
         address deployer = vm.addr(deployerPrivateKey);
 
         console.log("Deployer:", deployer);
-        console.log("Network ID:", NETWORK_ID);
+        console.log("Network ID:", networkId);
 
         vm.startBroadcast(deployerPrivateKey);
 
@@ -46,28 +58,33 @@ contract DeployAll is Script {
         postageStamp = address(postageContract);
         console.log("PostageStamp deployed at:", postageStamp);
 
-        // 3. Deploy PriceOracle
-        PriceOracle oracleContract = new PriceOracle(postageStamp);
-        priceOracle = address(oracleContract);
-        console.log("PriceOracle deployed at:", priceOracle);
+        // 3. Deploy StoragePriceOracle (for postage stamp pricing)
+        StoragePriceOracle storageOracleContract = new StoragePriceOracle(postageStamp);
+        storagePriceOracle = address(storageOracleContract);
+        console.log("StoragePriceOracle deployed at:", storagePriceOracle);
 
-        // 4. Deploy StakeRegistry
-        StakeRegistry stakeContract = new StakeRegistry(token, NETWORK_ID, priceOracle);
+        // 4. Deploy SwapPriceOracle (for SWAP protocol - this is what Bee nodes use)
+        SwapPriceOracle swapOracleContract = new SwapPriceOracle(SWAP_INITIAL_PRICE, SWAP_CHEQUE_VALUE_DEDUCTION);
+        swapPriceOracle = address(swapOracleContract);
+        console.log("SwapPriceOracle deployed at:", swapPriceOracle);
+
+        // 5. Deploy StakeRegistry
+        StakeRegistry stakeContract = new StakeRegistry(token, networkId, storagePriceOracle);
         stakeRegistry = address(stakeContract);
         console.log("StakeRegistry deployed at:", stakeRegistry);
 
-        // 5. Deploy Redistribution
-        Redistribution redistContract = new Redistribution(stakeRegistry, postageStamp, priceOracle);
+        // 6. Deploy Redistribution
+        Redistribution redistContract = new Redistribution(stakeRegistry, postageStamp, storagePriceOracle);
         redistribution = address(redistContract);
         console.log("Redistribution deployed at:", redistribution);
 
-        // 6. Deploy SimpleSwapFactory
+        // 7. Deploy SimpleSwapFactory
         SimpleSwapFactory factoryContract = new SimpleSwapFactory(token);
         swapFactory = address(factoryContract);
         console.log("SimpleSwapFactory deployed at:", swapFactory);
 
         // Configure roles
-        _configureRoles(postageContract, oracleContract, stakeContract);
+        _configureRoles(postageContract, storageOracleContract, stakeContract);
 
         vm.stopBroadcast();
 
@@ -75,21 +92,29 @@ contract DeployAll is Script {
         console.log("=== Deployment Complete ===");
         console.log("Token:", token);
         console.log("PostageStamp:", postageStamp);
-        console.log("PriceOracle:", priceOracle);
+        console.log("StoragePriceOracle:", storagePriceOracle);
+        console.log("SwapPriceOracle:", swapPriceOracle);
         console.log("StakeRegistry:", stakeRegistry);
         console.log("Redistribution:", redistribution);
         console.log("SimpleSwapFactory:", swapFactory);
+
+        // Note: Bee nodes should use SwapPriceOracle for BEE_PRICE_ORACLE_ADDRESS
+        console.log("");
+        console.log("For Bee node configuration:");
+        console.log("  BEE_PRICE_ORACLE_ADDRESS:", swapPriceOracle);
     }
 
-    function _configureRoles(PostageStamp postageContract, PriceOracle oracleContract, StakeRegistry stakeContract)
-        internal
-    {
+    function _configureRoles(
+        PostageStamp postageContract,
+        StoragePriceOracle storageOracleContract,
+        StakeRegistry stakeContract
+    ) internal {
         console.log("");
         console.log("Configuring roles...");
 
-        // Grant PriceOracle the PRICE_ORACLE_ROLE on PostageStamp
-        postageContract.grantRoles(priceOracle, postageContract.PRICE_ORACLE_ROLE());
-        console.log("Granted PRICE_ORACLE_ROLE to PriceOracle on PostageStamp");
+        // Grant StoragePriceOracle the PRICE_ORACLE_ROLE on PostageStamp
+        postageContract.grantRoles(storagePriceOracle, postageContract.PRICE_ORACLE_ROLE());
+        console.log("Granted PRICE_ORACLE_ROLE to StoragePriceOracle on PostageStamp");
 
         // Grant Redistribution the REDISTRIBUTOR_ROLE on PostageStamp
         postageContract.grantRoles(redistribution, postageContract.REDISTRIBUTOR_ROLE());
@@ -99,8 +124,8 @@ contract DeployAll is Script {
         stakeContract.grantRoles(redistribution, stakeContract.REDISTRIBUTOR_ROLE());
         console.log("Granted REDISTRIBUTOR_ROLE to Redistribution on StakeRegistry");
 
-        // Grant Redistribution the PRICE_UPDATER_ROLE on PriceOracle
-        oracleContract.grantRoles(redistribution, oracleContract.PRICE_UPDATER_ROLE());
-        console.log("Granted PRICE_UPDATER_ROLE to Redistribution on PriceOracle");
+        // Grant Redistribution the PRICE_UPDATER_ROLE on StoragePriceOracle
+        storageOracleContract.grantRoles(redistribution, storageOracleContract.PRICE_UPDATER_ROLE());
+        console.log("Granted PRICE_UPDATER_ROLE to Redistribution on StoragePriceOracle");
     }
 }
